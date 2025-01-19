9ccc69a264c412bcd54a7729ce6e95c53f2ac103
Евгений Семенов <116650874+Evgenij1234@users.noreply.github.com>
2025-01-19 19:44:00 +0700

    ++

--- Development/project_diploma_work/parser/run_scrapy.py
+++ Development/project_diploma_work/parser/run_scrapy.py
@@ -1,18 +1,10 @@
-from flask import Flask, jsonify
-import subprocess
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/run_scrapy', methods=['GET'])
 def run_scrapy():
-    # Запуск Scrapy спайдера
-    result = subprocess.run(['scrapy', 'crawl', 'your_spider_name'], capture_output=True, text=True)
-    
-    # Проверка результата выполнения
-    if result.returncode == 0:
-        return jsonify({'status': 'success', 'output': result.stdout})
-    else:
-        return jsonify({'status': 'error', 'output': result.stderr}), 500
+    return "Все окей"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
