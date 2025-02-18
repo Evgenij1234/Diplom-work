f4d7ce51006cd7f6d8e7edd075b5673e23773aa1
Евгений Семенов <116650874+Evgenij1234@users.noreply.github.com>
2025-02-18 09:25:31 +0700

    ++

--- Development/project_diploma_work/parser/run_scrapy.py
+++ Development/project_diploma_work/parser/run_scrapy.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/run_scrapy', methods=['GET'])
 def run_scrapy():
-    return "Все окей"
+    mas = []
+    for i in range(0,10,1):
+        mas[i] = "ха"
+    return mas
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
