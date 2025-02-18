c3e2dc2e17d803b5a80d9234f43f3bf29041afde
Евгений Семенов <116650874+Evgenij1234@users.noreply.github.com>
2025-02-18 09:59:39 +0700

    ++

--- Development/project_diploma_work/parser/catching_materials/catching_materials/settings.py
+++ Development/project_diploma_work/parser/catching_materials/catching_materials/settings.py
@@ -1,4 +1,3 @@
-# Настройки Scrapy
 BOT_NAME = "catching_materials"
 
 SPIDER_MODULES = ["catching_materials.spiders"]
@@ -8,7 +7,7 @@ ITEM_PIPELINES = {
     'catching_materials.pipelines.CatchingMaterialsPipeline': 1,
 }
 
-# robots.txt
+# Obey robots.txt rules
 ROBOTSTXT_OBEY =  False
 
 CONCURRENT_REQUESTS = 3
@@ -17,17 +16,3 @@ DOWNLOAD_DELAY = 3
 
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
-
-DOWNLOADER_MIDDLEWARES = {
-    "scrapy_playwright.middleware.PlaywrightMiddleware": 543,
-}
-
-# Настройки Playwright
-PLAYWRIGHT_BROWSER_TYPE = "chromium"
-PLAYWRIGHT_LAUNCH_OPTIONS = {
-    "headless": True,
-    "args": ["--no-sandbox", "--disable-setuid-sandbox"],
-}
-
-PLAYWRIGHT_DEFAULT_NAVIGATION_TIMEOUT = 30_000 
-
